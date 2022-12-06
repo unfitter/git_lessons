23ed92c1f8db2b8483b1c2ae5b64875c718b52c2
unfitter <82836438+unfitter@users.noreply.github.com>
2022-12-06 14:21:50 +0300

    mysql connected, do not work write func

--- check.php
+++ check.php
@@ -1,15 +1,15 @@
 <?php
-$name = filter_var(trim($_POST['name']),
-FILTER_SANITIZE_STRING);
+
+$login = filter_var(trim($_POST['login']));
+$name = filter_var(trim($_POST['name']));
 $email = filter_var(trim($_POST['email']),
-FILTER_SANITIZE_STRING);
-$login = filter_var(trim($_POST['login']),
-FILTER_SANITIZE_STRING);
-$password = filter_var(trim($_POST['password']),
-FILTER_SANITIZE_STRING);
+FILTER_SANITIZE_EMAIL);
+$password = filter_var(trim($_POST['password']));
+
 
 $mysql = new mysqli('localhost', 'root', '', 'Kursovaya');
+
 $mysql->query("INSERT INTO `Данные Клиента` (`login`, `name`, `email`, `password`)
-VALUES(`$login`, `$name`, `$email`, `$password`)" );
+VALUES('$login', '$name', '$email', '$password')" );
 $mysql->close();
 ?>
