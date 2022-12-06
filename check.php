5865ad2b9ece2ee6d756af090f1b825bee081a14
unfitter <82836438+unfitter@users.noreply.github.com>
2022-12-06 13:20:56 +0300

    reg try

--- check.php
+++ check.php
@@ -1,5 +1,15 @@
 <?php
-$name = $_POST['name'];
-echo $name;
+$name = filter_var(trim($_POST['name']),
+FILTER_SANITIZE_STRING);
+$email = filter_var(trim($_POST['email']),
+FILTER_SANITIZE_STRING);
+$login = filter_var(trim($_POST['login']),
+FILTER_SANITIZE_STRING);
+$password = filter_var(trim($_POST['password']),
+FILTER_SANITIZE_STRING);
 
+$mysql = new mysqli('localhost', 'root', '', 'Kursovaya');
+$mysql->query("INSERT INTO 'Данные Клиента' ('login', 'name', 'email', 'password')
+VALUES('$login', '$name', '$email', '$password')" );
+$mysql->close();
 ?>
