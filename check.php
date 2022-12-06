062dead97a8350d2dd5da6bc003354da4b85ee8e
unfitter <82836438+unfitter@users.noreply.github.com>
2022-12-06 20:37:29 +0300

    Fixed Registration page

--- check.php
+++ check.php
@@ -1,5 +1,7 @@
 <?php
-
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
 $login = filter_var(trim($_POST['login']));
 $name = filter_var(trim($_POST['name']));
 $email = filter_var(trim($_POST['email']),
@@ -9,7 +11,8 @@ $password = filter_var(trim($_POST['password']));
 
 $mysql = new mysqli('localhost', 'root', '', 'Kursovaya');
 
-$mysql->query("INSERT INTO `Данные Клиента` (`login`, `name`, `email`, `password`)
+$mysql->query("INSERT INTO `Данные Клиента` (`логин`, `Имя`, `Электронная почта`, `пароль`)
 VALUES('$login', '$name', '$email', '$password')" );
 $mysql->close();
+header('Location: /');
 ?>
