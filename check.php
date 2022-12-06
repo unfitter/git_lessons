e34d4b603f15aead0dd107f7d54dcfe4a14d51d5
unfitter <82836438+unfitter@users.noreply.github.com>
2022-12-06 21:09:58 +0300

    added checking for registration

--- check.php
+++ check.php
@@ -7,7 +7,18 @@ $name = filter_var(trim($_POST['name']));
 $email = filter_var(trim($_POST['email']),
 FILTER_SANITIZE_EMAIL);
 $password = filter_var(trim($_POST['password']));
+$password = md5($password);
 
+if (mb_strlen ($login) < 5 || mb_strlen($login) > 90) {
+    echo "Недоспутимая длина логина";
+    exit ();
+ } else if (mb_strlen($name) < 3 || mb_strlen ($name) > 50) {
+    echo "Недоспутимая длина имени";
+    exit();
+    } else if (mb_strlen($pass) < 2 || mb_strlen ($pass) > 6) {
+    echo "Недоспутимая длина пароля (от 2 до 6 символов)";
+    exit ();
+    }
 
 $mysql = new mysqli('localhost', 'root', '', 'Kursovaya');
 
