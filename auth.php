87b967d108f607d25965ec9e21515a16e6d7340d
unfitter <82836438+unfitter@users.noreply.github.com>
2022-12-06 22:09:26 +0300

    changed common to User Page

---
+++ auth.php
@@ -0,0 +1,19 @@
+<?php
+$login = filter_var(trim($_POST['login']));
+$password = filter_var(trim($_POST['password']));
+
+$mysql = new mysqli('localhost', 'root', '', 'Kursovaya');
+
+$result = $mysql->query("SELECT * FROM `Данные Клиента` WHERE `логин` = '$login' AND `пароль` = '$password'");
+$user = $result->fetch_assoc();
+if(count($user) == 0){
+    echo "Такого пользователя не существует!";
+    exit();
+}
+
+setcookie('user', $user['name'], time() + 3600, "/");
+
+$mysql->close();
+header('Location: /');
+
+?>
